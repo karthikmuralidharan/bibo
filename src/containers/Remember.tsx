088856dbf4cde3b7d3f44af34a2fffcc90861f44
Karthik Muralidharan <mkarthik91@gmail.com>
2019-02-16 00:01:23 +0530

    in progress; stateful

--- src/containers/Remember.tsx
+++ src/containers/Remember.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { State } from '../state';
-import { string } from 'prop-types';
 import { Image, ImageListResponse } from '../types/images';
 import GlobalProps from '../types/global';
 
+// @ts-ignore
+import Gallery from 'react-grid-gallery';
+import { OperationType } from '../state';
+
+interface GalleryDetail {
+    src: string;
+    thumbnail: string;
+    thumbnailWidth: number;
+    thumbnailHeight: number;
+    isSelected: boolean;
+}
+
 export default class Remember extends React.Component<GlobalProps, any> {
-    state: { err: Error | null; images: Image[] } = { err: null, images: [] };
+    state: { err: Error | null; images: GalleryDetail[] } = {
+        err: null,
+        images: [],
+    };
 
     constructor(props: GlobalProps) {
         super(props);
@@ -15,8 +28,17 @@ export default class Remember extends React.Component<GlobalProps, any> {
     componentWillMount = () => {
         fetch('/images/random')
             .then(res => {
-                res.json().catch((resp: ImageListResponse) => {
-                    this.state.images = resp.images;
+                res.json().then((resp: ImageListResponse) => {
+                    const images = resp.images.map(img => {
+                        return {
+                            src: img.url,
+                            thumbnail: img.url,
+                            thumbnailWidth: img.width,
+                            thumbnailHeight: img.height,
+                            isSelected: false,
+                        };
+                    });
+                    this.setState({ images: images });
                 });
             })
             .catch(e => {
@@ -24,7 +46,35 @@ export default class Remember extends React.Component<GlobalProps, any> {
             });
     };
 
+    private onSelect = (idx: number) => {
+        const selected = !this.state.images[idx].isSelected;
+        const img = this.state.images[idx];
+        this.state.images[idx].isSelected = selected;
+        this.setState(this.state);
+
+        let count = 1;
+        if (!selected) {
+            count = -1;
+        }
+
+        switch (this.props.context.status) {
+            case OperationType.BREATHE_IN_STARTED:
+                this.props.context.breatheIn.selected;
+                break;
+            case OperationType.BREATHE_OUT_STARTED:
+                this.props.context.breatheOut.selected;
+                break;
+        }
+    };
+
     render() {
-        return <div>hello</div>;
+        return (
+            <Gallery
+                images={this.state.images}
+                enableLightbox={false}
+                onSelectImage={this.onSelect}
+                margin={1}
+            />
+        );
     }
 }
