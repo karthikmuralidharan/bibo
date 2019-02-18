00ac7bc5e7ddd34a98cdfc84b4cbd8eb476ffc16
Karthik Muralidharan <mkarthik91@gmail.com>
2019-02-18 19:26:16 +0530

    add; remember screen

--- cmd/bibosvc/image.go
+++ cmd/bibosvc/image.go
@@ -15,7 +15,9 @@ type Image struct {
 }
 
 type ImageListResponse struct {
-	Images []Image `json:"images,omitempty"`
+	Images           []Image  `json:"images,omitempty"`
+	BreatheInMemory  []string `json:"breatheInMemory,omitempty"`
+	BreatheOutMemory []string `json:"breatheOutMemory,omitempty"`
 }
 
 func (rd *ImageListResponse) Render(w http.ResponseWriter, r *http.Request) error {
@@ -25,13 +27,23 @@ func (rd *ImageListResponse) Render(w http.ResponseWriter, r *http.Request) erro
 
 type imageFetcherFunc func(count int) ([]Image, error)
 
-// Shuffle shuffles around images for display
-func Shuffle(vals []Image) []Image {
+// ShuffleImages shuffles around images for display
+func ShuffleImages(vals []Image) []Image {
 	r := rand.New(rand.NewSource(time.Now().Unix()))
 	ret := make([]Image, len(vals))
 	perm := r.Perm(len(vals))
 	for i, randIndex := range perm {
 		ret[i] = vals[randIndex]
 	}
+	return ret
+}
+
+// ShuffleString shuffles around images for display
+func ShuffleString(vals []string) []string {
+	ret := make([]string, len(vals))
+	copy(ret, vals)
+	rand.Shuffle(len(ret), func(i, j int) {
+		ret[i], ret[j] = ret[j], ret[i]
+	})
 	return ret
 }
